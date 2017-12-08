ffb086eb3d89a89a2df8114bf9dfd6c501fc70ab
Chittaranjan Swaminathan <ksatyaki@gmail.com>
2017-12-08 17:08:31 +0100

    clang-formatted file.

--- include/smp/components/cost_evaluators/time.hpp
+++ include/smp/components/cost_evaluators/time.hpp
@@ -7,43 +7,36 @@
 
 #include <smp/components/cost_evaluators/base.hpp>
 
-
 using namespace std;
 
-
-template< class typeparams >
-int smp::cost_evaluator_time<typeparams>
-::ce_update_vertex_cost (vertex_t *vertex_in) {
+template <class typeparams>
+int smp::cost_evaluator_time<typeparams>::ce_update_vertex_cost(
+    vertex_t *vertex_in) {
 
   return 1;
 }
 
+template <class typeparams>
+int smp::cost_evaluator_time<typeparams>::ce_update_edge_cost(edge_t *edge_in) {
 
-template< class typeparams >
-int smp::cost_evaluator_time<typeparams>
-::ce_update_edge_cost (edge_t *edge_in) {
-  
   return 1;
 }
 
-
-
-template< class typeparams >
-double smp::cost_evaluator_time<typeparams>
-::evaluate_cost_trajectory (state_t *state_initial_in,
-			    trajectory_t *trajectory_in,
-			    state_t *state_final_in) {
+template <class typeparams>
+double smp::cost_evaluator_time<typeparams>::evaluate_cost_trajectory(
+    state_t *state_initial_in, trajectory_t *trajectory_in,
+    state_t *state_final_in) {
 
   double total_time = 0.0;
-  for (typename list<input_t*>::iterator iter = trajectory_in->list_inputs.begin();
+  for (typename list<input_t *>::iterator iter =
+           trajectory_in->list_inputs.begin();
        iter != trajectory_in->list_inputs.end(); iter++) {
-    
+
     input_t *input_curr = *iter;
-    
+
     total_time += (*input_curr)[0];
-    
   }
-  
+
   return total_time;
 };
 
